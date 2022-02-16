1f28d30366762ceef90f4f0b2904ec97eb15f586
Nathan Tarbert <66887028+NathanTarbert@users.noreply.github.com>
2022-02-16 07:59:33 -0800

    updated web3auth version to 2.4 current

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -11,7 +11,7 @@ async function main() {
   //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
-  // await hre.run('compile');
+  // await hre.run('compile');//
 
   // We get the contract to deploy
   const Greeter = await hre.ethers.getContractFactory("Greeter");

789d89715d286c1e2b8447bdcc7f6a5f5cc89a52
Jason Schwarz <jasonschwarz@Jasons-MacBook-Pro.local>
2022-03-23 16:14:41 -0300

    Update staking contract

--- migrations/3_deploy_staking.js
+++ migrations/3_deploy_staking.js
@@ -4,7 +4,7 @@ const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 module.exports = async function (deployer) {
   const name = "StakingToken";
   const symbol = "DKST";
-  const totalSupply = 1000000000;
+  const totalSupply = 10000;
   await deployProxy(StakingToken, [name, symbol, totalSupply], {
     deployer,
     initializer: "initialize",

150b6a5424e8e17c8b928b43c56dfc41ffa8ccbb
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-07-29 20:29:17 -0300

    Add logout button to landing page

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex justify-between text-blue-dark">
-      <div className="flex items-center">
-        <p className="mr-1">Powered by</p>
+    <div className="border-t border-indigo-500 ">
+      <div className="flex justify-center items-center text-blue-dark my-4">
+        {/* <div className="flex items-center"> */}
         <div className="flex">
           <a className="mr-3 flex" href="https://polygon.technology/">
             <Image src="/polygon.svg" alt="polygon" height={60} width={135} />
@@ -24,20 +24,23 @@ const Footer: React.FC = () => {
           </a>
         </div>
       </div>
-      <div className="flex items-center">
-        <p className="cursor-pointer ml-8">
-          <Link href="https://twitter.com/defikids_">
-            <a> Twitter - @defikids_</a>
-          </Link>
-        </p>
-        {/* <p className="cursor-pointer">FAQ</p> */}
 
-        <p className="cursor-pointer ml-8">
-          <Link href="https://learncrypto.com/glossary">
-            <a> Crypto Glossary</a>
-          </Link>
-        </p>
-        <p className="cursor-pointer ml-8"> DefiKids @ 2022</p>
+      <div className="flex justify-center text-blue-dark mx-4">
+        <div className="flex items-center">
+          <p className="cursor-pointer ml-8">
+            <Link href="https://twitter.com/defikids_">
+              <a> Twitter - @defikids_</a>
+            </Link>
+          </p>
+          {/* <p className="cursor-pointer">FAQ</p> */}
+
+          <p className="cursor-pointer ml-8">
+            <Link href="https://learncrypto.com/glossary">
+              <a> Crypto Glossary</a>
+            </Link>
+          </p>
+          <p className="cursor-pointer ml-8"> DefiKids @ 2022</p>
+        </div>
       </div>
     </div>
   );

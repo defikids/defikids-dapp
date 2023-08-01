a183ac94af25de9a6cd6d522390d149bd2f0df18
Nathan Tarbert <nathantarbert@Nathans-MBP-2.lan>
2023-07-31 22:25:30 -0400

    add faq list

---
+++ pages/data/faq.tsx
@@ -0,0 +1,122 @@
+//build out a list of faq's
+const faq_list = {
+  crypto: [
+    {
+      question: "Altcoin",
+      answer: "Any cryptocurrency other than Bitcoin.",
+    },
+    {
+      question: "Consensus Mechanism",
+      answer:
+        "A protocol used to achieve agreement among nodes in a distributed network, such as Proof-of-Work (PoW) or Proof-of-Stake (PoS).",
+    },
+    {
+      question: "Cryptocurrency",
+      answer:
+        "Digital or virtual currencies that use cryptography for security and operate on blockchain technology.",
+    },
+    {
+      question: "Decentralization",
+      answer:
+        "The distribution of power and control across a network of participants, rather than a central authority.",
+    },
+    {
+      question: "FOMO",
+      answer:
+        "Fear of Missing Out, a phenomenon where individuals rush to invest in a cryptocurrency due to the fear of missing potential gains.",
+    },
+    {
+      question: "FUD",
+      answer:
+        "Fear, Uncertainty, and Doubt, a tactic used to spread negative information or rumors about a cryptocurrency.",
+    },
+    {
+      question: "Halving",
+      answer:
+        "A programmed event in some cryptocurrencies, like Bitcoin, where the block reward for miners is cut in half, reducing the rate of new coin issuance.",
+    },
+    {
+      question: "ICO",
+      answer:
+        "Initial Coin Offering, a fundraising method where new cryptocurrencies are sold to investors before they are listed on exchanges.",
+    },
+    {
+      question: "NFT",
+      answer:
+        "Non-Fungible Token, a unique digital asset representing ownership or proof of authenticity of a specific item, often used for digital art and collectibles.",
+    },
+    {
+      question: "Tokenomics",
+      answer:
+        "The economic and monetary aspects of a cryptocurrency, including its distribution, supply, and use cases.",
+    },
+    {
+      question: "Transaction Fee",
+      answer:
+        "The fee paid by users to execute transactions on a blockchain network, often used to incentivize miners.",
+    },
+  ],
+  blockchain: [
+    {
+      question: "Blockchain",
+      answer:
+        "A distributed and decentralized ledger technology used to record transactions across multiple computers.",
+    },
+
+    {
+      question: "Distributed Ledger",
+      answer:
+        "A database that is shared and synchronized across multiple nodes in a network.",
+    },
+    {
+      question: "Immutable",
+      answer:
+        "Data on a blockchain that cannot be altered or deleted once it's recorded.",
+    },
+    {
+      question: "Mining",
+      answer:
+        "The process of validating transactions and adding them to the blockchain, typically performed by miners using computational power.",
+    },
+    {
+      question: "Nodes",
+      answer:
+        "Computers or devices participating in a blockchain network, each maintaining a copy of the entire blockchain.",
+    },
+    {
+      question: "Private Key",
+      answer:
+        "A secret cryptographic key used to sign transactions and gain access to a cryptocurrency wallet.",
+    },
+    {
+      question: "Smart Contracts",
+      answer:
+        "Self-executing contracts with terms directly written into code on a blockchain, automatically executing when predefined conditions are met.",
+    },
+  ],
+  staking: [
+    {
+      question: "Consensus Mechanism",
+      answer:
+        "A protocol used to achieve agreement among nodes in a distributed network, such as Proof-of-Work (PoW) or Proof-of-Stake (PoS).",
+    },
+    {
+      question: "",
+      answer: "",
+    },
+  ],
+  wallets: [
+    {
+      question: "Digital Wallet",
+      answer:
+        "Software or hardware used to store, manage, and interact with cryptocurrencies.",
+    },
+    {
+      question: "Wallet Address",
+      answer:
+        "A unique identifier used to receive or send cryptocurrencies, similar to an account number in traditional banking.",
+    },
+  ],
+};
+
+export default faq_list;

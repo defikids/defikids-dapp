e11d7e5873ad1714f7decc088fb9fdac1cb525d9
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-09-15 07:34:03 -0700

    Seperate src and app on the root

---
+++ app/resources/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Center, Flex, Box } from "@chakra-ui/react";
+import ResourcesModal from "@/components/modals/ResourcesModal";
+import Navbar from "@/components/Navbar";
+
+const Resources = () => {
+  return (
+    <Box height="100vh" bgGradient={["linear(to-b, black,#4F1B7C)"]}>
+      <Navbar />
+      <Center mt="1rem">
+        <Flex>
+          <ResourcesModal />
+        </Flex>
+      </Center>
+    </Box>
+  );
+};
+
+export default Resources;

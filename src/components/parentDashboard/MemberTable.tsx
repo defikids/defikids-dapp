6f5fa67df171e50ed4baed498ab60740500448f9
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-09-17 09:26:40 -0700

    Initial Parent Dashboard Setup

--- src/components/parentDashboard/MemberTable.tsx
+++ src/components/parentDashboard/MemberTable.tsx
@@ -9,6 +9,8 @@ import {
   useColorModeValue,
   Avatar,
   Text,
+  Heading,
+  Button,
 } from "@chakra-ui/react";
 import { SlOptions } from "react-icons/sl";
 
@@ -17,37 +19,61 @@ interface Notification {
   dateTime: string;
   userName: string;
   userAvatar: string;
-  isOnline: boolean;
 }
 
 const notifications: Notification[] = [
   {
-    notification: `It's <span style="font-weight: 600">Dan Abrahmov's</span> birthday. Wish him well!`,
-    dateTime: "2 days ago",
+    notification: `<span style="font-weight: 600">Dan Abrahmov</span> Updated Avatar.`,
+    dateTime: "September 10th at 9:10 AM",
     userName: "Dan Abrahmov",
     userAvatar: "https://bit.ly/dan-abramov",
-    isOnline: true,
   },
   {
-    notification: `<span style="font-weight: 600">Kent Dodds</span> liked your photo.`,
+    notification: `<span style="font-weight: 600">Kent Dodds</span> Staked 1.5 ETH.`,
     dateTime: "yesterday",
     userName: "Kent Dodds",
     userAvatar: "https://bit.ly/kent-c-dodds",
-    isOnline: true,
   },
   {
-    notification: `<span style="font-weight: 600">Jena Karlis</span> registered new client as <span style="font-weight: 600">Trilia</span>.`,
+    notification: `<span style="font-weight: 600">Jena Karlis</span> Timelocked 5 ETH.`,
     dateTime: "4 days ago",
     userName: "Jena Karlis",
     userAvatar:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
-    isOnline: false,
+  },
+  {
+    notification: `<span style="font-weight: 600">Jena Karlis</span> Staked 2 ETH.`,
+    dateTime: "4 days ago",
+    userName: "Jena Karlis",
+    userAvatar:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
+  },
+  {
+    notification: `<span style="font-weight: 600">Jena Karlis</span> Updated Avatar.`,
+    dateTime: "4 days ago",
+    userName: "Jena Karlis",
+    userAvatar:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
+  },
+  {
+    notification: `<span style="font-weight: 600">Kent Dodds</span> Claimed 1.5 ETH in staking rewards.`,
+    dateTime: "5 days ago",
+    userName: "Kent Dodds",
+    userAvatar: "https://bit.ly/kent-c-dodds",
   },
 ];
 
 const NotificationsList = () => {
   return (
-    <Container maxW="5xl" p={{ base: 5, md: 10 }}>
+    <Container maxW="5xl" bg={useColorModeValue("gray.100", "gray.900")}>
+      <Flex justify="space-between" my="1rem" align="center">
+        <Heading as="h3" size="sm" color="white">
+          Recent Activity
+        </Heading>
+        <Button size="xs" colorScheme="blue" variant="outline">
+          View All
+        </Button>
+      </Flex>
       <VStack
         boxShadow={useColorModeValue(
           "2px 6px 8px rgba(160, 174, 192, 0.6)",
@@ -57,6 +83,7 @@ const NotificationsList = () => {
         rounded="md"
         overflow="hidden"
         spacing={0}
+        my="2.5rem"
       >
         {notifications.map((notification, index) => (
           <Fragment key={index}>
@@ -64,37 +91,29 @@ const NotificationsList = () => {
               w="100%"
               justify="space-between"
               alignItems="center"
-              //   _hover={{ bg: useColorModeValue("gray.200", "gray.700") }}
+              _hover={{ bg: "gray.600" }}
+              cursor="pointer"
             >
               <Stack spacing={0} direction="row" alignItems="center">
                 <Flex p={4}>
                   <Avatar
                     size="md"
                     name={notification.userName}
-                    src={notification.userAvatar}
+                    src={notification.userAvatar || ""}
                   />
                 </Flex>
                 <Flex direction="column" p={2}>
                   <Text
-                    // color={useColorModeValue("black", "white")}
                     fontSize={{ base: "sm", sm: "md", md: "lg" }}
                     dangerouslySetInnerHTML={{
                       __html: notification.notification,
                     }}
                   />
-                  <Text
-                    // color={useColorModeValue("gray.400", "gray.200")}
-                    fontSize={{ base: "sm", sm: "md" }}
-                  >
+                  <Text fontSize={{ base: "sm", sm: "md" }}>
                     {notification.dateTime}
                   </Text>
                 </Flex>
               </Stack>
-              {notification.isOnline && (
-                <Flex p={4}>
-                  <Icon as={SlOptions} w={5} h={5} color="blue.400" />
-                </Flex>
-              )}
             </Flex>
             {notifications.length - 1 !== index && <Divider m={0} />}
           </Fragment>

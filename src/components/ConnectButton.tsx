2389090c4cf49727948457bae6ccca653604370b
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-10-06 10:31:41 -0300

    Show chain details conditionally

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useAuthStore } from "@/store/auth/authStore";
 import { Box, Button, Heading } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { usePathname } from "next/navigation";
+import shallow from "zustand/shallow";
 
 export const CustomConnectButton = () => {
   const pathname = usePathname();
+
+  const { fetchedUserDetails } = useAuthStore(
+    (state) => ({
+      fetchedUserDetails: state.fetchedUserDetails,
+    }),
+    shallow
+  );
   return (
     <ConnectButton.Custom>
       {({
@@ -58,43 +67,46 @@ export const CustomConnectButton = () => {
                   </Button>
                 );
               }
-              return (
-                // <> </>
+              return fetchedUserDetails ? (
                 <Box style={{ display: "flex", gap: 12 }}>
-                  <Button
-                    onClick={openChainModal}
-                    style={{ display: "flex", alignItems: "center" }}
-                    type="button"
-                  >
-                    {chain.hasIcon && (
-                      <Box
-                        style={{
-                          background: chain.iconBackground,
-                          width: 12,
-                          height: 12,
-                          borderRadius: 999,
-                          overflow: "hidden",
-                          marginRight: 4,
-                        }}
-                      >
-                        {chain.iconUrl && (
-                          <img
-                            alt={chain.name ?? "Chain icon"}
-                            src={chain.iconUrl}
-                            style={{ width: 12, height: 12 }}
-                          />
-                        )}
-                      </Box>
-                    )}
-                    {chain.name}
-                  </Button>
-                  <Button onClick={openAccountModal} type="button">
-                    {account.displayName}
-                    {account.displayBalance
-                      ? ` (${account.displayBalance})`
-                      : ""}
-                  </Button>
+                  <>
+                    <Button
+                      onClick={openChainModal}
+                      style={{ display: "flex", alignItems: "center" }}
+                      type="button"
+                    >
+                      {chain.hasIcon && (
+                        <Box
+                          style={{
+                            background: chain.iconBackground,
+                            width: 12,
+                            height: 12,
+                            borderRadius: 999,
+                            overflow: "hidden",
+                            marginRight: 4,
+                          }}
+                        >
+                          {chain.iconUrl && (
+                            <img
+                              alt={chain.name ?? "Chain icon"}
+                              src={chain.iconUrl}
+                              style={{ width: 12, height: 12 }}
+                            />
+                          )}
+                        </Box>
+                      )}
+                      {chain.name}
+                    </Button>
+                    <Button onClick={openAccountModal} type="button">
+                      {account.displayName}
+                      {account.displayBalance
+                        ? ` (${account.displayBalance})`
+                        : ""}
+                    </Button>
+                  </>
                 </Box>
+              ) : (
+                <></>
               );
             })()}
           </Box>

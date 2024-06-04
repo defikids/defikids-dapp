1a45fa7b1308f86199d4cbb4dba9d324667cfac7
Jason Schwarz <jason.c.schwarz@gmail.com>
2024-06-04 19:56:42 -0300

    Apply coming soon to main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { LandingPage } from "@/components/LandingPage";
 import { useEffect, useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
+import ComingSoon from "@/components/ComingSoon";
 
 export default function Page() {
   const [emailRoute, setEmailRoute] = useState("");
@@ -10,20 +11,23 @@ export default function Page() {
   const router = useRouter();
   const pathname = usePathname();
 
+  return <ComingSoon />;
+
+  //! This is the original code
   /*
    * This hook handle routing to the correct page after arriving from an email link
    */
-  useEffect(() => {
-    if (pathname?.startsWith("/member-invite")) {
-      setEmailRoute("member-invite");
-      router.push("/member-invite");
-    }
-    if (pathname?.startsWith("/confirm-email")) {
-      setEmailRoute("confirm-email");
-      router.push("/confirm-email");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (pathname?.startsWith("/member-invite")) {
+  //     setEmailRoute("member-invite");
+  //     router.push("/member-invite");
+  //   }
+  //   if (pathname?.startsWith("/confirm-email")) {
+  //     setEmailRoute("confirm-email");
+  //     router.push("/confirm-email");
+  //   }
+  // }, []);
 
-  if (emailRoute) return <></>;
-  return <LandingPage />;
+  // if (emailRoute) return <></>;
+  // return <LandingPage />;
 }
